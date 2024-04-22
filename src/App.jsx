d1bbe9f59ff5f30b726e84ce07ba9e9effc070d2
Alex Reinoso <ralexjessiel@gmail.com>
2024-04-21 21:55:55 -0500

    added supabase secrets

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient('https://znkpacfqnuhcosliyayx.supabase.co', process.env.REACT_APP_SUPABASE_ANON_KEY);
 
 function App() {
   
